ae85b719de1bd9b284a958e08562d83533d1ff0e
drizzer <drizzersilverberg@gmail.com>
2025-04-07 14:17:19 +0700

    add some opponents data

---
+++ data.py
@@ -0,0 +1,22 @@
+opponents = [
+    {
+        'id'                : 'yukum',
+        'name'              : 'Yu Kum',
+        'possible_dishes'   : [1,2,3,4,5,6,7],
+        'fixed_dishes'      : {
+            'starter'   : 'tomato_soup',
+            'main'      : None,
+            'dessert'   : None
+        }
+    },
+    {
+        'id'                : 'goetsu',
+        'name'              : 'Goetsu',
+        'possible_dishes'   : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+        'fixed_dishes'      : {
+            'starter'   : None,
+            'main'      : 'buttered_clams',
+            'dessert'   : None
+        }
+    },
+]
